8ab78c79ec3d1c18f0ef998eba3d0cd230673dad
TheArchSeal <simon.fikse0414@gmail.com>
2024-10-21 10:27:36 +0200

    run periodically

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import time
+import random
+from collections.abc import Callable
+
 # (question, [answers])
 type Question = tuple[str, list[float]]
 
 questions: list[Question] = [
-    ("Solve for x:\n\tx = 1 + 1\nx = ", [2])
+    ("Solve for x:\n\tx = 1 + 1\nx = ", [2]),
+    ("Solve for x:\n\tx * 2 = 3\nx = ", [1.5]),
 ]
+
+SLEEP_TIME: float = 60  # time in seconds between questions
 
 
 def give_question(q: Question) -> None:
@@ -21,9 +28,14 @@ def give_question(q: Question) -> None:
                 print("Incorrect, try again.\n")
 
 
+def run_periodically(f: Callable[[], None], T: float):
+    while True:
+        f()
+        time.sleep(T)
+
+
 def main() -> None:
-    for q in questions:
-        give_question(q)
+    run_periodically(lambda: give_question(random.choice(questions)), SLEEP_TIME)
 
 
 if __name__ == "__main__":
